a53634ad9ff3177f346ceb43d4c1e109df8a7d13
unknown <lacaze.jessica@gmail.com>
2015-01-15 16:00:22 +0100

    Initialisation

---
+++ WebApplication[Lacaze_Pintault]/WebApplication[Lacaze_Pintault]/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication_Lacaze_Pintault_
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ WebApplication[Lacaze_Pintault]/WebApplication[Lacaze_Pintault]/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebApplication_Lacaze_Pintault_.MvcApplication" Language="C#" %>
